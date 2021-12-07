d1ed3d9da36cb71c37ea0af805509719103bfa18
Katharina Hefele <katharina.hefele@gmx.de>
2021-12-07 20:55:21 +0100

    drag&drop

--- games/saveMona.js
+++ games/saveMona.js
@@ -1,6 +1,9 @@
 var leinwandwidth;
 var leinwandheight;
-const taktungszeit = 110;
+const taktungszeit = 30;
+
+var mouseOverBox = -1;
+var mouseOverFly = false; 
 
 
 class Fly {
@@ -84,10 +87,15 @@ $(document).ready(function() {
 
     var netImg = new Image();
     netImg.src="../images/net.png";
-    var watergun = new Image();
-    watergun.src="../images/wasserpistole.png";
-    var bucket = new Image();
-    bucket.src="../images/bucket.png";
+    var watergunImg = new Image();
+    watergunImg.src="../images/wasserpistole.png";
+    var crossHairImg = new Image();
+    crossHairImg.src="../images/fadenkreuz.png";
+    var bucketImg = new Image();
+    bucketImg.src="../images/bucket.png";
+
+    var heartImg = new Image();
+    heartImg.src="../images/herz.png";
 
 
     function zufallszahl(min, max){
@@ -104,11 +112,63 @@ $(document).ready(function() {
         context.drawImage(img, posX+5, posY+5, size-10, size-10);
     }
 
-    canvas.onmousemove=function( event ) {
-        var msg = "Handler for .mousemove() called at ";
-        msg += event.pageX + ", " + event.pageY;
-        console.log( msg );
-      };
+
+
+    //DRAG & DROP
+
+    function getMousePos(evt) {
+        
+        //Entfernung der canvas vom Rand muss noch abgezogen werden
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left-10, //10 wg. padding
+            y: evt.clientY - rect.top-10,
+          };
+
+    };
+
+    var mousePos; 
+    canvas.addEventListener('mousemove', function(evt) {
+
+        //check if mouse in Box
+        mousePos = getMousePos(evt);
+        // console.log(mousePos)
+        if (mousePos.x >= 530-3 && mousePos.x <= 590+3 && mousePos.y >= 10-3 && mousePos.y <= 70+3){ //3 wg. Rahmendicke
+            console.log("in der Box!")
+            mouseOverBox = 0;
+        } else if (mousePos.x >= 530-3 && mousePos.x <= 590+3 && mousePos.y >= 80-3 && mousePos.y <= 140+3){
+            console.log("in der Box 1!")
+            mouseOverBox = 1;
+        } else if (mousePos.x >= 530-3 && mousePos.x <= 590+3 && mousePos.y >= 150-3 && mousePos.y <= 210+3){
+            console.log("in der Box 2!")
+            mouseOverBox = 2;
+        } else {
+            mouseOverBox = -1;
+        }
+        
+    }, false);
+
+
+    var dragObject = null;
+    
+    document.onmousedown = function (event) {
+        if (mouseOverBox == 0) {
+            dragObject = netImg;
+            console.log("auf Netz geklickt");
+            //if (dragParent != null) { dragParent.style.transition = "none"; }
+        } else if (mouseOverBox == 1) {
+            dragObject = crossHairImg;
+        } else if (mouseOverBox == 2) {
+            dragObject = bucketImg;
+        }
+    }
+
+    document.onmouseup = function (event) {
+        //evt.stopImmediatePropagation();
+        //if (dragParent != null) { dragParent.style.transition = null;}
+        dragObject = null;
+        
+    }
 
     var count = 0;
     //Taktung
@@ -117,23 +177,29 @@ $(document).ready(function() {
         count++;
         context.clearRect(0, 0, leinwandwidth, leinwandheight);
         context.drawImage(mona,100,100,200,200); 
+        context.drawImage(heartImg,10,10,20,20); 
+        context.drawImage(heartImg,30,10,20,20); 
+        context.drawImage(heartImg,50,10,20,20); 
 
         imgWithOutline(netImg, 530, 10, 60);
-        imgWithOutline(watergun, 530, 80, 60);
-        imgWithOutline(bucket, 530, 150, 60);
+        imgWithOutline(watergunImg, 530, 80, 60);
+        imgWithOutline(bucketImg, 530, 150, 60);
 
 
         for (var x = 0; x < flyArray.length; x++){
             flyArray[x].update();
         }
-        if (count%40==0){
+        if (count%200==0){
             var fliege3 = new Fly([zufallszahl(0, leinwandwidth),zufallszahl(0, leinwandheight)], [zufallszahl(120,260), zufallszahl(120,260)], zufallszahl(1000,4000), flyimg);
             flyArray.push(fliege3);  
         }
 
         smoker1.update();
 
-        
+        if (dragObject != null){
+            context.drawImage(dragObject, mousePos.x-25, mousePos.y-25, 50, 50);
+        }
+          
     }
 
 
