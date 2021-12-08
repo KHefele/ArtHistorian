17e588f22e2a3435cd17625d1583c9ade4aed901
Katharina Hefele <katharina.hefele@gmx.de>
2021-12-08 12:55:27 +0100

    head, contact formular, save Mona (air conditioning, tools, photographer, red flies when hover)

--- games/saveMona.js
+++ games/saveMona.js
@@ -4,13 +4,15 @@ const taktungszeit = 30;
 
 var mouseOverBox = -1;
 var mouseOverFly = false; 
+var hearts = 2;
 
 
 class Fly {
-    constructor(start, end, movetime, flyimage) {
+    constructor(start, end, movetime, flyimage, size) {
         this.end = end;
         this.movetime = movetime;
         this.flyimage = flyimage;
+        this.size = size;
 
         var percent = taktungszeit/this.movetime;
         this.xSchrittlänge = (end[0]-start[0])*percent;
@@ -31,8 +33,16 @@ class Fly {
             this.position[0] = this.end[0];
             this.position[1] = this.end[1];
         }
-        context.drawImage(this.flyimage,this.position[0],this.position[1],20,20); 
+        context.drawImage(this.flyimage,this.position[0],this.position[1],this.size,this.size); 
         
+    }
+
+    hoverWithNet(currentDragObj, wantedDragObj, mouseX, mouseY){
+        if (currentDragObj == wantedDragObj && mouseX >= this.position[0] && mouseX <= this.position[0]+this.size && mouseY >= this.position[1] && mouseY <= this.position[1]+this.size){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -64,6 +74,34 @@ class Smoker {
     }
 }
 
+class Photographer {
+    constructor(startX, endX, photographerimage) {
+        this.endX = endX;
+        this.photographerimage = photographerimage;
+
+        this.movetime = 5000;
+
+        var percent = taktungszeit/this.movetime;
+        this.xSchrittlänge = (endX-startX)*percent;
+
+        this.positionX = startX;
+        this.lifetime = 0;
+
+    }
+
+    update(){
+        this.lifetime += taktungszeit;
+
+        if (this.lifetime <= this.movetime){
+            this.positionX += this.xSchrittlänge;   
+        } else {
+            this.positionX = this.endX;
+        }
+        context.drawImage(this.photographerimage,this.positionX,300,150,150); 
+        
+    }
+}
+
 $(document).ready(function() {
 
     //Spielbrett
@@ -73,17 +111,23 @@ $(document).ready(function() {
 
     context = canvas.getContext('2d');
 
-    var mona = new Image();
-    mona.src = "../images/canvas.png";
+    //Bilder
+    var monaImg = new Image();
+    monaImg.src = "../images/canvas.png";
 
-    var flyimg = new Image();
-    flyimg.src = "../images/fly.png";
+    var flyImg = new Image();
+    flyImg.src = "../images/fly.png";
+    var flyImgRed = new Image();
+    flyImgRed.src = "../images/flyRed.png";
     var flyArray = [];
 
     var smokerimg = new Image();
     smokerimg.src="../images/smoker.png";
     var smoker1 = new Smoker(0, 200, smokerimg);
 
+    var photographerImg = new Image();
+    photographerImg.src="../images/photographer.png";
+    var photographer1 = new Photographer(800, 400, photographerImg);
 
     var netImg = new Image();
     netImg.src="../images/net.png";
@@ -96,6 +140,11 @@ $(document).ready(function() {
 
     var heartImg = new Image();
     heartImg.src="../images/herz.png";
+
+    var airConditioning = new Image();
+    airConditioning.src="../images/klimaanlage.png";
+    var toolsImg = new Image();
+    toolsImg.src="../images/werkzeug.png";
 
 
     function zufallszahl(min, max){
@@ -134,7 +183,9 @@ $(document).ready(function() {
         mousePos = getMousePos(evt);
         // console.log(mousePos)
         if (mousePos.x >= 530-3 && mousePos.x <= 590+3 && mousePos.y >= 10-3 && mousePos.y <= 70+3){ //3 wg. Rahmendicke
-            console.log("in der Box!")
+            console.log("in der Box!");
+
+            //CURSOR ÄNDERN
             mouseOverBox = 0;
         } else if (mousePos.x >= 530-3 && mousePos.x <= 590+3 && mousePos.y >= 80-3 && mousePos.y <= 140+3){
             console.log("in der Box 1!")
@@ -166,7 +217,14 @@ $(document).ready(function() {
     document.onmouseup = function (event) {
         //evt.stopImmediatePropagation();
         //if (dragParent != null) { dragParent.style.transition = null;}
+        for (var x = 0; x < flyArray.length; x++){
+            
+            if (flyArray[x].hoverWithNet(dragObject, netImg, mousePos.x, mousePos.y)){
+                flyArray.splice(x, 1);
+            }
+        }
         dragObject = null;
+        
         
     }
 
@@ -176,25 +234,41 @@ $(document).ready(function() {
     function taktung() {
         count++;
         context.clearRect(0, 0, leinwandwidth, leinwandheight);
-        context.drawImage(mona,100,100,200,200); 
-        context.drawImage(heartImg,10,10,20,20); 
-        context.drawImage(heartImg,30,10,20,20); 
-        context.drawImage(heartImg,50,10,20,20); 
+        context.drawImage(monaImg,200,100,200,200);
+        context.drawImage(airConditioning,10,40,100,100); 
 
+        context.fillText("20° 50%", 50, 84);
+
+        var heartsx = 10;
+        for (var i = 0; i<=hearts; i++){
+            context.drawImage(heartImg,heartsx,10,20,20); 
+            heartsx += 20;  
+        }
+        
         imgWithOutline(netImg, 530, 10, 60);
         imgWithOutline(watergunImg, 530, 80, 60);
         imgWithOutline(bucketImg, 530, 150, 60);
+        imgWithOutline(toolsImg, 530, 220, 60);
 
-
+        //Flies
         for (var x = 0; x < flyArray.length; x++){
             flyArray[x].update();
+            if (flyArray[x].hoverWithNet(dragObject, netImg, mousePos.x, mousePos.y)){
+                flyArray[x].flyimage = flyImgRed;
+                // flyArray[x].size = 35;
+                //flyArray[x].position[0] -= 20;
+            } else {
+                flyArray[x].flyimage = flyImg;
+                // flyArray[x].size = 20;
+            }
         }
         if (count%200==0){
-            var fliege3 = new Fly([zufallszahl(0, leinwandwidth),zufallszahl(0, leinwandheight)], [zufallszahl(120,260), zufallszahl(120,260)], zufallszahl(1000,4000), flyimg);
+            var fliege3 = new Fly([zufallszahl(0, leinwandwidth),zufallszahl(0, leinwandheight)], [zufallszahl(120,260), zufallszahl(120,260)], zufallszahl(1000,4000), flyImg, 35);
             flyArray.push(fliege3);  
         }
 
         smoker1.update();
+        photographer1.update();
 
         if (dragObject != null){
             context.drawImage(dragObject, mousePos.x-25, mousePos.y-25, 50, 50);
--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-    $("video").on("mouseover", function(event) {
-      this.play();
-    }).on('mouseout', function(event) {
-      this.pause();
-    });
+
+
+  $("#iconEmailFormular").click(function(){
+    $("#emailFormular").css("display", "block");
   })
+
+  $("#iconQuellenangabe").click(function(){
+    $("#quellenangabe").css("display", "block");
+  })
+
+  $(window).click(function(event){
+    if (event.target == document.getElementById("quellenangabe")){
+      $("#quellenangabe").css("display", "none");
+    }
+    if (event.target == document.getElementById("emailFormular")){
+      $("#emailFormular").css("display", "none");
+    }
+  })
+
+
+
+  $("video").on("mouseover", function(event) {
+    this.play();
+  }).on('mouseout', function(event) {
+    this.pause();
+  });
+
+
+
+})
